30a0fd190f03328ddefe764f8a5b28b951bd3eba
Vlad Faizov <32297717+thedipperproduction@users.noreply.github.com>
2018-03-26 09:52:00 +0500

    Main files

---
+++ bad_index.php
@@ -0,0 +1,78 @@
+//////////////////////////////
+///IT'S RUSSIAN INDEX PAGE!///
+//////////////////////////////
+
+<?php 
+require_once('config.php');
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>URL4ME</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+			<?php 
+
+if(isset($_POST['submit']) && $_POST['url-shorter'] != ""){
+
+//generate shor link;
+
+$title = generateRandomString();
+
+//add https:// to user input
+
+if(substr($_POST['url-shorter'],0,8) != 'https://'){
+	
+	$url = 'https://'.$_POST['url-shorter'];
+}else{
+	$url = $_POST['url-shorter'];
+}
+
+	//insert link into our database
+
+	$result ="";
+	$result = $db->prepare("INSERT INTO urls VALUES('',?,?)");
+
+	$result->bind_param("ss",$url,$title);
+	
+	$result->execute();
+	echo "<p class='alert alert-success text-center'>Твой URL готов</p>";
+	$result = $db->prepare("SELECT * FROM urls WHERE short_url=?");
+
+	$result->bind_param("s",$title);
+	
+	$result->execute();
+
+	$goto = $result->get_result()->fetch_array();
+	//print_r($goto[2]);
+	 $surl ='domain.com'.$goto[2];
+	 echo 'Сокращённый URL -  '. $surl;
+}else{
+	$error = 'Пожалуйста, введите ссылку';
+	echo $error;
+}
+			?>
+				<form action="" method="post">
+				<div class="form-group">
+					<label for="url">Url</label>
+					<input type="text" class="form-control" name="url-shorter">
+					<p>Пожалуйста, не пишите с http:// или https://</p>
+				</div>
+				<input type="submit" name="submit" placeholder="Вставьте ссылку для сокращения..">
+				</form>
+			</div>
+			
+		</div>
+	</div>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
